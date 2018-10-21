b64ca5cb1e1713cae30499ed2b9d2a162f2cd569
adrian <bella.adus@gmail.com>
2018-10-21 23:11:32 +0200

    refactor models to Sequential

--- student_model.py
+++ student_model.py
@@ -1,6 +1,6 @@
-from keras import Input, Model
-from keras.layers import Convolution2D
+from keras.layers import Conv2D
 from keras.layers import Dense, Flatten
+from keras.models import Sequential
 
 
 class StudentCNN:
@@ -10,26 +10,18 @@ class StudentCNN:
         self.model = self._build_model()
 
     def _build_model(self):
-        input = Input((1, 200, 200))
+        model = Sequential()
 
-        #  Add convolutional and normalization layers
-        hidden_conv_layer_1 = Convolution2D(32, (8, 8), strides=(4, 4), padding='valid', activation='relu',
-                                            data_format='channels_first')(input)
-        hidden_conv_layer_2 = Convolution2D(64, (4, 4), strides=(2, 2), padding='valid', activation='relu')(
-            hidden_conv_layer_1)
-        hidden_conv_layer_3 = Convolution2D(64, (3, 3), strides=(1, 1), padding='valid', activation='relu')(
-            hidden_conv_layer_2)
-        # -------------------------------------------
+        model.add(Conv2D(32, (8, 8), strides=(4, 4), padding='valid', activation='relu',
+                         data_format='channels_first', input_shape=(1, 200, 200)))
+        model.add(Conv2D(64, (4, 4), strides=(2, 2), padding='valid', activation='relu'))
+        model.add(Conv2D(64, (3, 3), strides=(1, 1), padding='valid', activation='relu'))
 
         # make convolution layers falttend (1 dimensional)
-        flattened = Flatten()(hidden_conv_layer_3)
-        #  add FC layers
-        hidden_fc_layer = Dense(self.hidden_fc_size, activation='softmax')(flattened)
-        output_layer = Dense(self.action_size)(hidden_fc_layer)
-        # -------------------------------------------
+        model.add(Flatten())
 
-        # configure learning process and initialize model
-        model = Model(inputs=input, output=output_layer)
+        model.add(Dense(self.hidden_fc_size, activation='relu'))
+        model.add(Dense(self.action_size, activation='softmax'))
 
         model.compile(optimizer='adam',
                       loss='categorical_crossentropy',
