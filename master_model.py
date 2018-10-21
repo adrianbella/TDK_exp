038821a62d3f89bb3ab4ba166f424d85c10a4514
adrian <bella.adus@gmail.com>
2018-10-21 23:42:31 +0200

    adding learning rate config to student_model

--- master_model.py
+++ master_model.py
@@ -1,5 +1,4 @@
-from keras import Input, Model
-from keras.layers import Convolution2D, Conv2D
+from keras.layers import Conv2D
 from keras.layers import Dense, Flatten
 from keras.models import Sequential
 
